6ec4c7458da7b835dde3ea269c372158bad122b4
Mattias Fredsberg <mattias.fredsberg@etteplan.com>
2017-11-22 14:52:40 +0100

    Code cleanup

--- sketch.js
+++ sketch.js
@@ -1,75 +1,71 @@
-var planets = []
+let planets
 
 function setup() {
   createCanvas(windowWidth, windowHeight)
   init(2)
 }
 
-function init(n) {  
+function init(n) {
   planets = []
   colorMode(RGB)
   background(128)
-  for (var i = 0; i < n; i++) {
-    var pos = p5.Vector.fromAngle(i * TWO_PI / n).setMag(100)
-    var v = p5.Vector.fromAngle(i * TWO_PI / n + HALF_PI).setMag(n / 2.0)
-    var c = i * 255.0 / n
+  for (let i = 0; i < n; i++) {
+    const angle = map(i, 0, n, 0, TWO_PI)
+    const pos = p5.Vector.fromAngle(angle).setMag(100)
+    const v = p5.Vector.fromAngle(angle + HALF_PI).setMag(n / 2.0)
+    const c = map(i, 0, n, 0, 255)
     planets.push(new Planet(pos, v, c))
   }
-  setTimeout(function() {
-    init(n + 1)  
+  setTimeout(function () {
+    init(n + 1)
   }, 10000);
 }
 
 function draw() {
-//  colorMode(RGB)
-//  background(128, 128, 128, 5)
   push()
   translate(width / 2, height / 2)
   update()
 
-  for (var i = 0; i < planets.length; i++) {
-    planets[i].render()
-  }
+  planets.forEach(p => p.render())
   pop()
 }
 
 function update() {
-  for (var i = 0; i < planets.length; i++) {
-    for (var j = 0; j < planets.length; j++) {
-      if (i != j) {
-        var v = p5.Vector.sub(planets[i].pos, planets[j].pos)
-        var r = v.mag()
-        var f = 1000 / (r * r)
-        planets[i].applyForce(v.setMag(-f))
-      }
+  planets.forEach(p1 => planets.forEach(p2 => {
+    if (p1 != p2) {
+      const v = p5.Vector.sub(p1.pos, p2.pos)
+      const r = v.mag()
+      const f = 1000 / (r * r)
+      p1.applyForce(v.setMag(-f))
     }
-  }
-  for (var k = 0; k < planets.length; k++) {
-    planets[k].update()
-  }
+  }))
+  planets.forEach(p => p.update())
 }
 
-function Planet(pos, v, c) {
-  this.pos = pos
-  this.v = v
-  this.c = c
-  this.r = 5;
-  this.force = createVector()
-  
-  this.render = function() {
+class Planet {
+  constructor(pos, v, c) {
+    this.pos = pos
+    this.v = v
+    this.c = c
+    this.r = 5;
+    this.force = createVector()
+  }
+
+  render() {
     colorMode(HSB)
-    stroke((c + 128) % 255, 255, 255)
-    fill(c, 255, 255)
+    stroke((this.c + 128) % 255, 255, 255)
+    fill(this.c, 255, 255)
     ellipse(this.pos.x, this.pos.y, this.r * 2, this.r * 2)
   }
-  
-  this.update = function() {
+
+  update() {
     this.v.add(this.force)
+    this.pos.add(this.v)
+
     this.force = createVector()
-    this.pos.add(v)
   }
-  
-  this.applyForce = function(f) {
+
+  applyForce(f) {
     this.force.add(f)
   }
 }
